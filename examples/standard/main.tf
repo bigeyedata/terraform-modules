cc2850f102cb7b1444ee4707a88ee59eb9e97c2d
Brian Kopp <brian@bigeye.com>
2023-12-13 16:22:59 -0700

    docs: update version for docs to latest release

--- examples/standard/main.tf
+++ examples/standard/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v0.2.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v0.2.2"
   environment = "test"
   instance    = "bigeye"
 
