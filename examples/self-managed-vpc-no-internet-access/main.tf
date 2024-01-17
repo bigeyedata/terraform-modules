fffe0ed3253f9dece350b78cad95a65eb7fef039
David Nguyen <david@bigeye.com>
2024-01-16 16:13:37 -0800

    fix: normalize subnet names
    Use a standard set of subnet names that match the byovpc subnet
    variables to reduce confusion.  Overall these are easier to understand
    for new users
    
    - public
    - internal
    - application

--- examples/self-managed-vpc-no-internet-access/main.tf
+++ examples/self-managed-vpc-no-internet-access/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v1.6.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v1.6.1"
   environment = local.environment
   instance    = local.instance
 
