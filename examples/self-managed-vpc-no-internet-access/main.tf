2f1281404d7eaea07be52802c6cb62c6a4024c9d
deeno35 <deeno35@users.noreply.github.com>
2024-01-23 19:59:05 +0000

    Update example docs to use new version v1.9.0

--- examples/self-managed-vpc-no-internet-access/main.tf
+++ examples/self-managed-vpc-no-internet-access/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v1.8.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v1.9.0"
   environment = local.environment
   instance    = local.instance
 
