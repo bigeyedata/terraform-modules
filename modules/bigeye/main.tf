3d3f5a9e76ed6f155dec7360e7c05956c98ffe33
Brian Kopp <brian@bigeye.com>
2024-01-31 08:32:07 -0700

    fix: remove environment vars that can just be injected

--- modules/bigeye/main.tf
+++ modules/bigeye/main.tf
@@ -961,8 +961,6 @@ module "web" {
       APP_ENVIRONMENT   = var.environment
       NODE_ENV          = "production"
       PORT              = var.web_port
-      INTERCOM_APP_ID   = ""
-      HEAP_API_KEY      = ""
       DROPWIZARD_HOST   = "https://${local.datawatch_dns_name}"
       DATAWATCH_ADDRESS = "https://${local.datawatch_dns_name}"
       MAX_NODE_MEM_MB   = "4096"
