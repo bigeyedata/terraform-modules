97a2dd0862fc8f1084288aff2bca229793fb5aae
Brian Kopp <brian@bigeye.com>
2024-02-12 14:21:34 -0700

    docs: add docs for alarms module

---
+++ examples/alarms/main.tf
@@ -0,0 +1,23 @@
+module "bigeye" {
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v2.2.0"
+  environment = "test"
+  instance    = "bigeye"
+
+  # Your parent DNS name here, e.g. bigeye.my-company.com
+  top_level_dns_name = ""
+
+  # Get this from Bigeye Sales
+  image_tag = ""
+}
+
+# Alarms module
+module "alarms" {
+  source                           = "git::https://github.com/bigeyedata/terraform-modules//modules/alarms?ref=v2.2.0"
+  stack                            = module.bigeye.stack_name
+  datawatch_rds_identifier         = module.bigeye.datawatch_rds_identifier
+  datawatch_rds_replica_identifier = module.bigeye.datawatch_rds_replica_identifier
+  temporal_rds_identifier          = module.bigeye.temporal_rds_identifier
+  rabbitmq_name                    = module.bigeye.rabbitmq_name
+  redis_cluster_id                 = module.bigeye.redis_cluster_id
+}
+
