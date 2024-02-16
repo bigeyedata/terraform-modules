558929ae6a6ff7d12adb0637d051a091d484512a
Brian Kopp <brian@bigeye.com>
2024-02-16 12:41:35 -0700

    feat: add outputs from service module

--- modules/simpleservice/outputs.tf
+++ modules/simpleservice/outputs.tf
@@ -1,6 +1,21 @@
+output "ecs_service_arn" {
+  description = "The ARN of the ecs service"
+  value       = aws_ecs_service.this.id
+}
+
 output "dns_name" {
   description = "The DNS name of the load balancer"
   value       = aws_lb.this.dns_name
+}
+
+output "load_balancer_full_name" {
+  description = "The load balancer full name, for use with CW metrics"
+  value       = aws_lb.this.arn_suffix
+}
+
+output "target_group_full_name" {
+  description = "Target group full name, for use with CW metrics"
+  value       = aws_lb_target_group.this.arn_suffix
 }
 
 output "zone_id" {
