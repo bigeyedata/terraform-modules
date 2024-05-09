4751503e426a121505cc6f3b1fc763e47c5d155d
BrianKopp <BrianKopp@users.noreply.github.com>
2024-05-09 12:34:50 +0000

    chore(release): update example docs to use new version v4.2.0

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.1.2"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.2.0"
   environment = "test"
   instance    = "bigeye"
 
