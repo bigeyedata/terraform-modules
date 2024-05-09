5d2412a8745af9811d74228b6354309532e05aae
BrianKopp <BrianKopp@users.noreply.github.com>
2024-05-09 12:34:50 +0000

    chore(release): update example docs to use new version v4.2.0

--- examples/self-managed-vpc-no-internet-access/main.tf
+++ examples/self-managed-vpc-no-internet-access/main.tf
@@ -79,7 +79,7 @@ provider "aws" {
 }
 
 module "bigeye" {
-  source             = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.1.2"
+  source             = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.2.0"
   environment        = local.environment
   instance           = local.instance
   top_level_dns_name = local.subdomain
