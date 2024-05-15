afa1c1055c265004520cc299aa11fe6643904c40
deeno35 <deeno35@users.noreply.github.com>
2024-05-15 19:05:19 +0000

    chore(release): update example docs to use new version v4.7.1

--- examples/self-managed-dns/main.tf
+++ examples/self-managed-dns/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.7.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.7.1"
   environment = local.environment
   instance    = local.instance
 
--- examples/self-managed-mtls-certs/main.tf
+++ examples/self-managed-mtls-certs/main.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "ca_bundle" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.7.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v4.7.1"
   environment = "test"
   instance    = "bigeye"
 
