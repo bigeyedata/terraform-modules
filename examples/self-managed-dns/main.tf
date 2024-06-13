1128c6449ef3fb0f12131d3b25ec4e8a7147355b
deeno35 <deeno35@users.noreply.github.com>
2024-06-13 20:38:17 +0000

    chore(release): update example docs to use new version v6.3.1

--- examples/self-managed-dns/main.tf
+++ examples/self-managed-dns/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v6.3.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v6.3.1"
   environment = local.environment
   instance    = local.instance
 
