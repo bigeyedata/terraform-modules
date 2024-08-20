81bd3363f98de046e3b0904cd393a3a963f135df
jfcole <jfcole@users.noreply.github.com>
2024-08-20 00:59:53 +0000

    chore(release): update example docs to use new version v10.4.1

--- examples/self-managed-ecr/main.tf
+++ examples/self-managed-ecr/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v10.4.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v10.4.1"
   environment = "test"
   instance    = "bigeye"
 
