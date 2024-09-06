9d9628c1eb31128b8a797f4821f467b3df4c007b
deeno35 <deeno35@users.noreply.github.com>
2024-09-06 17:57:51 +0000

    chore(release): update example docs to use new version v11.1.0

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v11.0.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v11.1.0"
   environment = "test"
   instance    = "bigeye"
 
--- examples/self-managed-dns/main.tf
+++ examples/self-managed-dns/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v11.0.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v11.1.0"
   environment = local.environment
   instance    = local.instance
 
