fc78259345cff15cddd52a2024c6a55ace9ea788
deeno35 <deeno35@users.noreply.github.com>
2024-10-31 20:21:41 +0000

    chore(release): update example docs to use new version v11.6.0

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v11.5.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v11.6.0"
   environment = "test"
   instance    = "bigeye"
 
