40c15d0ca556088833f7d0d6ffca77750bd8d8f0
roman-bigeye <187668753+roman-bigeye@users.noreply.github.com>
2025-02-22 00:52:14 +0000

    chore(release): update example docs to use new version v17.9.0

--- examples/self-managed-mail-server/main.tf
+++ examples/self-managed-mail-server/main.tf
@@ -3,7 +3,7 @@ data "aws_secretsmanager_secret" "byomailserver_smtp_password" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.8.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.9.0"
   environment = "test"
   instance    = "bigeye"
 
