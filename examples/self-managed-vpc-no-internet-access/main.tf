3f8daa14ac45e80b2bc7ed087f1cd7db981419c6
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-03-21 18:19:46 +0000

    chore(release): update example docs to use new version v17.13.1

--- examples/self-managed-vpc-no-internet-access/main.tf
+++ examples/self-managed-vpc-no-internet-access/main.tf
@@ -95,7 +95,7 @@ module "bringyourown" {
 }
 
 module "bigeye" {
-  source             = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.13.0"
+  source             = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.13.1"
   environment        = local.environment
   instance           = local.instance
   top_level_dns_name = local.domain_name
