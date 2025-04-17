c625ff6c311c4de8c5ddebb1642d69a4e579c97e
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-04-17 23:38:05 +0000

    chore(release): update example docs to use new version v17.21.1

--- examples/self-managed-ecr/main.tf
+++ examples/self-managed-ecr/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.21.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.21.1"
   environment = "test"
   instance    = "bigeye"
 
--- examples/self-managed-iam/main.tf
+++ examples/self-managed-iam/main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.21.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.21.1"
   environment = local.environment
   instance    = local.instance
 
