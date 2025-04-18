98da68ac237a1ccf3602031390662a3a37a949ac
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-04-18 20:40:39 +0000

    chore(release): update example docs to use new version v17.22.0

--- examples/self-managed-mtls-certs/main.tf
+++ examples/self-managed-mtls-certs/main.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "ca_bundle" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.21.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v17.22.0"
   environment = "test"
   instance    = "bigeye"
 
