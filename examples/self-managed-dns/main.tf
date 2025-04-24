3f9eb19039b304401e3547a837d470376b9201ba
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-04-24 22:54:34 +0000

    chore(release): update example docs to use new version v19.0.2

--- examples/self-managed-dns/main.tf
+++ examples/self-managed-dns/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v19.0.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v19.0.2"
   environment = local.environment
   instance    = local.instance
 
