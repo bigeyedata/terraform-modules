0a57c6640416cb601486f06a1aef6b9812ed8eb8
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-05-05 21:29:30 +0000

    chore(release): update example docs to use new version v20.4.0

--- examples/self-managed-vpc/main.tf
+++ examples/self-managed-vpc/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.3.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.4.0"
   environment = "test"
   instance    = "bigeye"
 
