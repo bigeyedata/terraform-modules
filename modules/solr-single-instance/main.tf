bigeyedata/terraform-modules

commit 9cd05e1a04a42c20e1d482016c80ff605288349c

feat: increase solr os ebs volume size and fix ebs tags
The OS root EBS volume can get uncomfortably full at
30GB because solr logs are quite high volume.

We are also missing tags on the OS EBS volume

modules/solr-single-instance/main.tf
@@ -59,6 +59,22 @@ resource "aws_launch_template" "solr" {
   metadata_options {
     http_tokens = "required"
   }
+
+  ebs_optimized = true
+  block_device_mappings {
+    ebs {
+      encrypted   = "true"
+      volume_size = var.ebs_volume_size_os
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(local.solr_tags, {
+      Name = "${var.name}-os"
+    })
+  }
 }
 
 resource "aws_autoscaling_group" "solr" {

modules/solr-single-instance/variables.tf
@@ -57,10 +57,16 @@ variable "availability_zone" {
   type        = string
 }
 
+variable "ebs_volume_size_os" {
+  type    = string
+  default = 40
+}
+
 variable "ebs_volume_size" {
   type    = string
   default = 100
 }
+
 variable "ebs_volume_iops" {
   description = "Set iops to the value supported by your instance type. https://docs.aws.amazon.com/ec2/latest/instancetypes/gp.html"
   type        = number

commit d28d5f64ddca69afa99e481ea53dd0da4b8c66d0

fix: fix missing launch template ebs device name
This is a bug introduced in https://github.com/bigeyedata/terraform-modules/pull/509

Without this, the autoscaling group fails to refresh and leaves
solr in a down state.

modules/solr-single-instance/main.tf
@@ -62,6 +62,7 @@ resource "aws_launch_template" "solr" {
 
   ebs_optimized = true
   block_device_mappings {
+    device_name = "/dev/xvda"
     ebs {
       encrypted   = "true"
       volume_size = var.ebs_volume_size_os