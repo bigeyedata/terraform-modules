9d141b5f3403c963fdc952cef7c0c14d1dab1133
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-05-30 23:41:55 +0000

    chore(release): update example docs to use new version v20.8.1

--- examples/self-managed-iam/main.tf
+++ examples/self-managed-iam/main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.8.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.8.1"
   environment = local.environment
   instance    = local.instance
 
--- examples/self-managed-mail-server/main.tf
+++ examples/self-managed-mail-server/main.tf
@@ -3,7 +3,7 @@ data "aws_secretsmanager_secret" "byomailserver_smtp_password" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.8.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.8.1"
   environment = "test"
   instance    = "bigeye"
 
