f4297274320e688b21afd1867b874ba801cd81f5
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-06-02 18:28:21 +0000

    chore(release): update example docs to use new version v20.9.1

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.9.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.9.1"
   environment = "test"
   instance    = "bigeye"
 
--- examples/self-managed-vpc-no-internet-access/main.tf
+++ examples/self-managed-vpc-no-internet-access/main.tf
@@ -95,7 +95,7 @@ module "bringyourown" {
 }
 
 module "bigeye" {
-  source             = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.9.0"
+  source             = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v20.9.1"
   environment        = local.environment
   instance           = local.instance
   top_level_dns_name = local.domain_name
