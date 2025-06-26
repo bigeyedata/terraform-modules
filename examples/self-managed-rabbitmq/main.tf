85090fd83a9a84122f675e2e4afc8a381e7686a0
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-06-26 16:00:45 +0000

    chore(release): update example docs to use new version v21.4.1

--- examples/self-managed-rabbitmq/main.tf
+++ examples/self-managed-rabbitmq/main.tf
@@ -8,7 +8,7 @@ locals {
 }
 
 module "bigeye" {
-  source = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.4.0"
+  source = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.4.1"
 
   environment = local.environment
   instance    = local.instance
