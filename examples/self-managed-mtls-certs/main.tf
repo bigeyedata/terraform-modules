30623fd24ffdf7689e366b6549dcea14a4bb8b58
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-08-07 21:32:05 +0000

    chore(release): update example docs to use new version v21.7.0

--- examples/self-managed-mtls-certs/main.tf
+++ examples/self-managed-mtls-certs/main.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "ca_bundle" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.6.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.7.0"
   environment = "test"
   instance    = "bigeye"
 
--- examples/standard/main.tf
+++ examples/standard/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.6.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.7.0"
   environment = "test"
   instance    = "bigeye"
 
