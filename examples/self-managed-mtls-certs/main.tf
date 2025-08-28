eaefd44b2fca877ea88dc8b49c63355cd60df7d1
David Nguyen <david@bigeye.com>
2025-08-28 09:31:50 -0700

    chore(release): update example docs to use new version v21.9.0 manual fixup
    manual fixup due to concurrent deploy failure

--- examples/self-managed-mtls-certs/main.tf
+++ examples/self-managed-mtls-certs/main.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "ca_bundle" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.8.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.9.0"
   environment = "test"
   instance    = "bigeye"
 
