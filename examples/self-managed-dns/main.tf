593c9c6e60d3e28326e4fbb0a35f5ba0376dc3cf
kevin-bigeye <229997515+kevin-bigeye@users.noreply.github.com>
2025-09-04 17:14:36 +0000

    chore(release): update example docs to use new version v22.0.0

--- examples/self-managed-dns/main.tf
+++ examples/self-managed-dns/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v21.9.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v22.0.0"
   environment = local.environment
   instance    = local.instance
 
