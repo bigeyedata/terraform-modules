4387e8cfa39b214a3489880e59e182f8c0dc8793
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-09-25 19:15:57 +0000

    chore(release): update example docs to use new version v23.1.2

--- examples/self-managed-dns/main.tf
+++ examples/self-managed-dns/main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v23.1.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v23.1.2"
   environment = local.environment
   instance    = local.instance
 
--- examples/self-managed-ecr/main.tf
+++ examples/self-managed-ecr/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v23.1.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v23.1.2"
   environment = "test"
   instance    = "bigeye"
 
