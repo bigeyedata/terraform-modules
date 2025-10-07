5d2bb306748cde7dd62396f95c35cc52ca95566b
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-10-07 18:51:45 +0000

    chore(release): update example docs to use new version v24.0.0

--- examples/self-managed-mtls-certs/main.tf
+++ examples/self-managed-mtls-certs/main.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "ca_bundle" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v23.2.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v24.0.0"
   environment = "test"
   instance    = "bigeye"
 
--- examples/self-managed-vpc/main.tf
+++ examples/self-managed-vpc/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v23.2.1"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v24.0.0"
   environment = "test"
   instance    = "bigeye"
 
