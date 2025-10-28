9db3ec862486665be2110c2fa19dcb6fe593e5f3
kevin-bigeye <229997515+kevin-bigeye@users.noreply.github.com>
2025-10-28 21:57:50 +0000

    chore(release): update example docs to use new version v25.2.0

--- examples/self-managed-mail-server/main.tf
+++ examples/self-managed-mail-server/main.tf
@@ -3,7 +3,7 @@ data "aws_secretsmanager_secret" "byomailserver_smtp_password" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.1.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.2.0"
   environment = "test"
   instance    = "bigeye"
 
