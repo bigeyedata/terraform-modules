addc05664c328f93d4669da857775ff19329377b
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-11-05 19:22:55 +0000

    chore(release): update example docs to use new version v25.3.0

--- examples/self-managed-mail-server/main.tf
+++ examples/self-managed-mail-server/main.tf
@@ -3,7 +3,7 @@ data "aws_secretsmanager_secret" "byomailserver_smtp_password" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.2.2"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.3.0"
   environment = "test"
   instance    = "bigeye"
 
