995ec940cdb318b56757a97a8687e4603b9e513a
deeno35 <2043289+deeno35@users.noreply.github.com>
2025-12-02 23:14:59 +0000

    chore(release): update example docs to use new version v25.4.4

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.4.3"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.4.4"
   environment = "test"
   instance    = "bigeye"
 
