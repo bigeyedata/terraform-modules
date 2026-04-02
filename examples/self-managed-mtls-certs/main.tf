6e984a9e050e4bff85cd86e1bf92bf03cf1943fe
jfcole <52989146+jfcole@users.noreply.github.com>
2026-04-02 23:37:03 +0000

    chore(release): update example docs to use new version v25.13.0

--- examples/self-managed-mtls-certs/main.tf
+++ examples/self-managed-mtls-certs/main.tf
@@ -15,7 +15,7 @@ data "aws_secretsmanager_secret" "ca_bundle" {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.12.2"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.13.0"
   environment = "test"
   instance    = "bigeye"
 
--- examples/self-managed-rabbitmq/main.tf
+++ examples/self-managed-rabbitmq/main.tf
@@ -8,7 +8,7 @@ locals {
 }
 
 module "bigeye" {
-  source = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.12.2"
+  source = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.13.0"
 
   environment = local.environment
   instance    = local.instance
