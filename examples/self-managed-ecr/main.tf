21792334858f6c87e6bc37dadd5a633c03c6ac21
mhalverson <3054280+mhalverson@users.noreply.github.com>
2026-06-29 19:03:06 +0000

    chore(release): update example docs to use new version v25.22.0

--- examples/self-managed-ecr/main.tf
+++ examples/self-managed-ecr/main.tf
@@ -1,5 +1,5 @@
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.21.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.22.0"
   environment = "test"
   instance    = "bigeye"
 
