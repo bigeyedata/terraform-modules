fa7c9e652d271670ac05364f11054104077328f2
esmioley <5316217+esmioley@users.noreply.github.com>
2026-07-30 19:05:31 +0000

    chore(release): update example docs to use new version v25.25.1

--- examples/self-managed-iam/main.tf
+++ examples/self-managed-iam/main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "bigeye" {
-  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.25.0"
+  source      = "git::https://github.com/bigeyedata/terraform-modules//modules/bigeye?ref=v25.25.1"
   environment = local.environment
   instance    = local.instance
 
